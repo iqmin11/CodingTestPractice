4a181604cc3612564ee26eb4e3b109ff656a70eb
승민 황 <iqmin11@gmail.com>
2023-10-26 22:47:34 +0900

    황승민 : 동전0, 체육복 풀이

---
+++ Practice/동전0/동전0.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	std::pair<int, int> Ex = std::pair<int, int>();
+	std::vector<int> ExCoinType = std::vector<int>();
+	
+	std::cin >> Ex.first;
+	std::cin >> Ex.second;
+
+	ExCoinType.resize(10);
+
+	for (size_t i = 0; i < ExCoinType.size(); i++)
+	{
+		std::cin >> ExCoinType[i];
+	}
+
+	auto StartIter = ExCoinType.rbegin();
+	auto EndIter = ExCoinType.rend();
+	int Answer = 0;
+
+	for (; StartIter != EndIter; StartIter++)
+	{
+		if (*StartIter > Ex.second)
+		{
+			continue;
+		}
+
+		if (*StartIter == 0)
+		{
+			continue;
+		}
+
+		int Quotient = Ex.second / *StartIter;
+		int Mod = Ex.second % *StartIter;
+
+		Answer += Quotient;
+		Ex.second = Mod;
+
+		if (Ex.second == 0)
+			break;
+	}
+
+	std::cout << Answer;
+
+	return 0;
+}
+
+/*while (Ex.second >= *StartIter)
+{
+	Ex.second -= *StartIter;
+	Answer++;
+	if (Ex.second == 0)
+		break;
+}*/
---
+++ Practice/체육복/체육복.cpp
@@ -0,0 +1,99 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+enum class StudentState
+{
+    Normal,
+    Lost,
+    Reserve
+};
+
+class Student
+{
+public:
+    StudentState State = StudentState::Normal;
+};
+
+int solution(int n, vector<int> lost, vector<int> reserve)
+{
+    std::vector<Student> Students = std::vector<Student>();
+    Students.resize(n);
+
+    int AbleStudent = n;
+
+	std::sort(lost.begin(), lost.end(), [](int a, int b) { return a < b; });
+
+    for (size_t i = 0; i < reserve.size(); i++)
+    {
+		int StudentIndex = reserve[i] - 1;
+        Students[StudentIndex].State = StudentState::Reserve;
+    }
+
+    for (size_t i = 0; i < lost.size(); i++)
+    {
+		int StudentIndex = lost[i] - 1;
+        if (Students[StudentIndex].State == StudentState::Normal)
+        {
+            Students[StudentIndex].State = StudentState::Lost;
+			AbleStudent--;
+		}
+		else if ((Students[StudentIndex].State == StudentState::Reserve))
+		{
+			Students[StudentIndex].State = StudentState::Normal;
+			lost[i] = -99;
+        }
+    }
+
+	for (size_t i = 0; i < lost.size(); i++)
+	{
+		if (lost[i] == -99)
+		{
+			continue;
+		}
+		int StudentIndex = lost[i] - 1;
+		int PrevIndex = StudentIndex - 1;
+		int PostIndex = StudentIndex + 1;
+
+		if (PrevIndex >= 0)
+		{
+			if (Students[PrevIndex].State == StudentState::Reserve)
+			{
+				Students[PrevIndex].State = StudentState::Normal;
+				Students[StudentIndex].State = StudentState::Normal;
+				AbleStudent++;
+			}
+			else if (PostIndex < Students.size())
+			{
+				if (Students[PostIndex].State == StudentState::Reserve)
+				{
+					Students[PostIndex].State = StudentState::Normal;
+					Students[StudentIndex].State = StudentState::Normal;
+					AbleStudent++;
+				}
+			}
+		}
+		else if (PostIndex < Students.size())
+		{
+			if (Students[PostIndex].State == StudentState::Reserve)
+			{
+				Students[PostIndex].State = StudentState::Normal;
+				Students[StudentIndex].State = StudentState::Normal;
+				AbleStudent++;
+			}
+		}
+	}
+
+    return AbleStudent;
+}
+
+int main()
+{
+    int a = solution(5, {4,2}, {3,5});
+    return 0;
+}
+
+
+
