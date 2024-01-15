22044217e9a5483899b7c272b379f67954090724
승민 황 <iqmin11@gmail.com>
2024-01-15 17:12:58 +0900

    황승민 : 집가서 파일명 정리

---
+++ Practice/파일명 정렬/파일 명 정렬.cpp
@@ -0,0 +1,79 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+std::vector<std::string> Division(std::string _FileName)
+{
+    std::vector<std::string> result;
+    result.reserve(3);
+    int Check = 0;
+    for (int i = 0; i < _FileName.size(); ++i)
+    {
+        if (result.empty())
+        {
+            if (_FileName[i] >= '0' && _FileName[i] <= '9')
+            {
+                Check = i;
+                result.push_back(_FileName.substr(0, Check));
+            }
+        }
+        else if (!(_FileName[i] >= '0' && _FileName[i] <= '9'))
+        {
+            result.push_back(_FileName.substr(Check, i - Check));
+            result.push_back(_FileName.substr(i, _FileName.size() - 1));
+            break;
+        }
+        else if(i == _FileName.size() - 1 && result.size() != 3)
+        {
+            result.push_back(_FileName.substr(Check, i - Check + 1));
+            result.push_back("\0");
+        }
+    }
+
+    for (size_t i = 0; i < result.size(); i++)
+    {
+        if (i == 1)
+        {
+            continue;
+        }
+
+        for (size_t j = 0; j < result[i].size(); j++)
+        {
+            result[i][j] = toupper(result[i][j]);
+        }
+    }
+    return result;
+}
+
+vector<string> solution(vector<string> files)
+{
+    std::stable_sort(files.begin(), files.end(), [](std::string _Left, std::string _Right)
+        {
+            std::vector<std::string> LeftResult = Division(_Left);
+            std::vector<std::string> RightResult = Division(_Right);
+
+            if (LeftResult[0] != RightResult[0])
+            {
+                return LeftResult[0] < RightResult[0];
+            }
+
+            int LeftNumber = std::stoi(LeftResult[1]);
+            int RightNumber = std::stoi(RightResult[1]);
+
+            if (LeftNumber != RightNumber)
+            {
+                return LeftNumber < RightNumber;
+            }
+            return false;
+        });
+
+    return files;
+}
+
+int main()
+{
+    solution({ "A500001", "A50000" });
+
+}
