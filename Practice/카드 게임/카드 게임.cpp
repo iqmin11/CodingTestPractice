1084a481ea72c480569679c59dc0afe9cdad0a82
승민 황 <iqmin11@gmail.com>
2024-04-30 16:16:22 +0900

    황승민 : 미니맥스 일시 중단

--- Practice/카드 게임/카드 게임.cpp
+++ Practice/카드 게임/카드 게임.cpp
@@ -1,6 +1,42 @@
 //巩力 林家 : https://www.acmicpc.net/problem/11062
 
 #include <iostream>
+#include <vector>
+
+int DP[1001][1001];
+bool MyTurn = false;
+
+int MiniMaxTree(int CurLeft, int CurRight, const std::vector<int>& Board)
+{
+	MyTurn = !MyTurn;
+
+	if (CurLeft > CurRight)
+	{
+		return 0;
+	}
+
+	if (DP[CurLeft][CurRight] != 0)
+	{
+		return DP[CurLeft][CurRight];
+	}
+
+	if (MyTurn) //辟快
+	{
+		int LeftPick = Board[CurLeft] + MiniMaxTree(CurLeft + 1, CurRight, Board);
+		int RightPick = Board[CurRight] + MiniMaxTree(CurLeft, CurRight - 1, Board);
+
+		DP[CurLeft][CurRight] = std::max(LeftPick, RightPick);
+	}
+	else
+	{
+		int LeftPick = MiniMaxTree(CurLeft + 1, CurRight, Board);
+		int RightPick = MiniMaxTree(CurLeft, CurRight - 1, Board);
+
+		DP[CurLeft][CurRight] = std::min(LeftPick, RightPick);
+	}
+
+	return DP[CurLeft][CurRight];
+}
 
 int main()
 {
@@ -15,13 +51,17 @@ int main()
 	{
 		int CardSize;
 		std::cin >> CardSize;
+		std::vector<int> Board;
+		Board.resize(CardSize);
 
 		for (int i = 0; i < CardSize; i++)
 		{
-			int Score;
-			std::cin >> Score;
+			std::cin >> Board[i];
 		}
 
+		MiniMaxTree(0, CardSize - 1, Board);
+		int Answer = DP[0][CardSize - 1];
+		int a = 0;
 	}
 
 	return 0;
