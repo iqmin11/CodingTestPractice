646686de8223ae8f0ad215a3eaf86f0b29444e1d
승민 황 <iqmin11@gmail.com>
2024-05-02 14:12:32 +0900

    황승민 : 코테 복습 완

--- Practice/줄세우기/줄세우기.cpp
+++ Practice/줄세우기/줄세우기.cpp
@@ -1,8 +1,10 @@
 //문제 주소 : https://www.acmicpc.net/problem/2631
 
 #include <iostream>
+#include <vector>
 
-int DP[201]; //DP[a] = b : a인덱스에 있는 값을 선택했을 때 증가하는 부분수열의 크기는 b
+std::vector<int> Arr;
+int DP[201]; //DP[a] = b: Arr[a] 까지의 증가하는 부분수열의 크기는 b
 
 int main()
 {
@@ -11,9 +13,30 @@ int main()
 	std::cout.tie(NULL);
 
 	int N;
-
 	std::cin >> N;
+	Arr.resize(N + 1);
+	
+	for (int i = 1; i <= N; i++)
+	{
+		std::cin >> Arr[i];
+	}
 
+	int Answer = INT32_MIN;
+	
+	DP[0] = 0;
+	for (int i = 1; i <= N; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			if (Arr[i] > Arr[j])
+			{
+				DP[i] = std::max(DP[i], DP[j] + 1);
+			}
+		}
+		Answer = std::max(Answer, DP[i]);
+	}
+
+	std::cout << N - Answer;
 
 	return 0;
 }
