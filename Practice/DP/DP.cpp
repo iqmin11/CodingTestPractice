77c445e3afb9320c0af4104a6451398ada55ea8a
승민 황 <iqmin11@gmail.com>
2024-06-30 16:56:49 +0900

    황승민 : 가장 긴 증가하는 부분수열, 공통 부분 문자열 정리

--- Practice/DP/DP.cpp
+++ Practice/DP/DP.cpp
@@ -25,6 +25,7 @@ std::vector<Load> LoadsInfo = { { 0,0 }, //dummy
 	{3, 6}, //3번짐
 	{5, 12} //4번짐
 }; 
+//Answer : 14
 
 int BackpackProblem()
 {
@@ -41,8 +42,7 @@ int BackpackProblem()
 	{
 		for (int w = 1; w <= BackPack; w++) //남은 무게
 		{
-			//남은 무게에 비해서 짐의 무게가 무거울 때
-			if (LoadsInfo[i].Weight > w)
+			if (LoadsInfo[i].Weight > w) //남은 무게에 비해서 짐의 무게가 무거울 때
 			{
 				DP[i][w] = DP[i - 1][w];
 			}
@@ -59,14 +59,88 @@ int BackpackProblem()
 	return DP[LoadCount][BackPack];
 }
 
-//가장 긴 증가하는 부분수열
+//가장 긴 증가하는 부분수열 
+//응용문제로는 줄세우기가 있다.
+//해당 유형의 문제는, 불규칙한 수열이 주어진다
+std::vector<int> Arr = { 12,3,4,5,22,15,2,16,17,1,50,6 };
+//Answer : 7
+
+int LIS()
+{
+	//DP배열을 만들고, Arr상 앞 숫자들과 크기 비교를 한다
+	std::vector<int> DP(Arr.size(), 1);
+
+	for (size_t CurIndex = 0; CurIndex < Arr.size(); CurIndex++)
+	{
+		for (size_t PrevIndex = 0; PrevIndex < CurIndex; PrevIndex++)
+		{
+			//두 숫자를 비교해서 오름차순이라면
+			if (Arr[CurIndex] > Arr[PrevIndex])
+			{
+				//누가 더 긴지 비교를 하고, 더 긴 경우를 저장한다
+				DP[CurIndex] = std::max(DP[CurIndex], DP[PrevIndex] + 1);
+			}
+		}
+	}
+
+	int Answer = INT32_MIN;
+	for (auto i : DP)
+	{
+		Answer = std::max(Answer, i);
+	}
+
+	return Answer;
+}
 
 //공통 부분 문자열
+//해당 유형은 두개의 문자열이 주어지고, 연속된 부분문자열 중 가장 긴 공통 부분문자열을 찾는 것이다.
+
+std::string Str1 = "aaaaabbbcccdd";
+std::string Str2 = "asdasdabbbccasdasd"; 
+//Answer : 6
+
+int LCPS()
+{
+	//Init
+	std::vector<std::vector<int>> DP;
+	DP.resize(Str1.size());
+	for (size_t i = 0; i < Str1.size(); i++)
+	{
+		DP[i].resize(Str2.size(), 0);
+	}
+	
+	//Sol
+	int Answer = INT32_MIN;
+
+	for (int y = 0; y < DP.size(); y++)
+	{
+		for (int x = 0; x < DP[y].size(); x++)
+		{
+			if (Str1[y] == Str2[x])
+			{
+				if (x == 0 || y == 0)
+				{
+					DP[y][x] = 1;
+				}
+				else
+				{
+					DP[y][x] = DP[y - 1][x - 1] + 1;
+				}
+
+				Answer = std::max(Answer, DP[y][x]);
+			}
+		}
+	}
+
+	return Answer;
+}
 
 //동전 문제
 
 int main()
 {
 	std::cout << BackpackProblem() << std::endl;
+	std::cout << LIS() << std::endl;
+	std::cout << LCPS() << std::endl;
 	return 0;
 }
