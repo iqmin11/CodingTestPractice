445fbd2457606f91aac3563eec5345a434085f6d
승민 황 <iqmin11@gmail.com>
2024-07-23 15:49:59 +0900

    황승민 : C# 공부 시작

---
+++ Practice/BasicTemplate/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+//Before .NET 5 Version Template
+
+//using System;
+
+//namespace MyApp // Note: actual namespace depends on the project name.
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Hello World!");
+//        }
+//    }
+//}
+
+//공부해야 할 키워드
+//1. C#에서의 namespace
+//2. C#에서의 class
+//3. C#에서의 접근제한지정자 키워드와 사용처
+//4. using 키워드
+//5. 진입점과 진입점 인자
+//6. 자료구조
+//7. static 키워드
+//8. Console class
--- Practice/피보나치 수열/연습장.cpp
+++ Practice/피보나치 수열/연습장.cpp
@@ -1,56 +1,8 @@
 #include <iostream>
-#include <vector>
-
-#define ll long long
-
-std::vector<std::vector<int>> Image = 
-{ 
-	{1,2,3},
-	{4,5,6},
-	{7,8,9}
-};
-
-void DebugRender()
-{
-	std::cout << "////////////////////////////" << std::endl;
-
-	for (int i = 0; i < Image.size(); i++)
-	{
-		for (int j = 0; j < Image[i].size(); j++)
-		{
-			std::cout << Image[i][j];
-		}
-		std::cout << std::endl;
-	}
-
-	std::cout << "////////////////////////////" << std::endl;
-}
-
-void RotateRect(std::vector<std::vector<int>>& Img)
-{
-	std::vector<std::vector<int>> temp;
-	temp.resize(Img.size());
-
-	for (int y = 0; y < temp.size(); y++)
-	{
-		temp[y].resize(Img[y].size(), 0);
-	}
-
-	for (int y = 0; y < Img.size(); y++)
-	{
-		for (int x = 0; x < Img[y].size(); x++)
-		{
-			temp[x][temp.size() - 1 - y] = Img[y][x];
-		}
-	}
-
-	Img = temp;
-}
 
 int main()
 {
-	RotateRect(Image);
-	DebugRender();
-	
+	std::cout << "Hello, World!";
+
     return 0;
 }
